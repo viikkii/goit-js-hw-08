d5c39b52223b2ca33c1052e5b650bfd457480457
Viktoriia Fokina <viikkii.f@gmail.com>
2021-11-09 00:22:17 +0200

    ДЗ-08_Обновления_

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createElementsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createElementsMarkup(galleryItems) {
+    return galleryItems
+        .map(({ original, preview, description }) => {
+            return `
+            <a class="gallery__item" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" /></a>
+              `;
+        }).join("")
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', captionDelay: 250, captionPosition: 'bottom'
+});
+
+lightbox.on('show.simplelightbox', function () {
+   console.log(`hi`)
+});
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+console.log(galleryItems);
 
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createElementsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createElementsMarkup(galleryItems) {
+    return galleryItems
+        .map(({ original, preview, description }) => {
+            return `
+            <a class="gallery__item" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" /></a>
+              `;
+        }).join("")
+};
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, captionPosition: 'bottom'});
+console.log(galleryMarkup);
