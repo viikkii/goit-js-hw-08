d82584847ae2b5f684505078b6361ef7c3048652
Viktoriia Fokina <viikkii.f@gmail.com>
2021-11-10 23:54:53 +0200

    ДЗ-08
    Подготовила ДЗ-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,34 @@
-console.log(galleryItems);
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
-const galleryContainer = document.querySelector(".gallery");
-const galleryMarkup = createElementsMarkup(galleryItems);
-galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+// создание нового слушателя
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
 
-function createElementsMarkup(galleryItems) {
-    return galleryItems
-        .map(({ original, preview, description }) => {
-            return `
-            <a class="gallery__item" href="${original}">
-         <img class="gallery__image" src="${preview}" alt="${description}" /></a>
-              `;
-        }).join("")
-};
+// старт видео 
+const onPlay = function(data) {
+    console.log('Played the video!')};
+player.on('play', onPlay);
 
-const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, captionPosition: 'bottom'});
-console.log(galleryMarkup);
+// получаем название видеоролика
+player.getVideoTitle().then(function(title) {
+     console.log('title:', title);
+});
+
+// отслеживания события
+function TimeUpdateNow(evt) {
+    localStorage.setItem(LOCALSTORAGE_KEY, evt.seconds);
+    console.log(`Video now:  ${evt.seconds}`); 
+}
+player.on('timeupdate', throttle(TimeUpdateNow, 2000));
+
+// получить текущее время
+const saveCurrentTime = player.setCurrentTime(0)
+    .then(function (seconds) {
+    console(saveCurrentTime)
+})
+    .catch(function (error) {
+   console.log(`Error!`)
+    }
+);
