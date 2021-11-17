65f0e1c67faaf0fb85fa5e10b290fde01d2ba318
Viktoriia Fokina <viikkii.f@gmail.com>
2021-11-17 15:32:39 +0200

    ДЗ-08
    правки к ДЗ-08, задача 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,22 +1,41 @@
 import throttle from 'lodash.throttle';
 
+// Обьявляемый переменные
 const LOCALSTORAGE_KEY = "feedback-form-state";
 const form = document.querySelector(".feedback-form");
-const formFilter = {};
+let formFilter = {};
 
-form.addEventListener("change", evt => {
+// вешаем слушателя на форму
+form.addEventListener("input", throttle(inputForm, 500))
+function inputForm(evt) {
     formFilter[evt.target.name] = evt.target.value;
-    console.log(formFilter);
-    localStorage.setItem('formFilter', JSON.stringify(formFilter));
-});
-
-form.addEventListener("submit", saveEmail);
-
-function saveEmail(evt) {
-  evt.preventDefault();
-    console.log(`email :`, form.elements.email.value);
-    console.log(`message :`, form.elements.message.value);
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formFilter)); 
 }
 
-localStorage.removeItem("formFilter");
+// получение данных из локального хранилище
+initForm()
+function initForm() {
+  let persistedFilters = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (persistedFilters) {
+    persistedFilters = JSON.parse(persistedFilters)
+    Object.entries(persistedFilters).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    })
+  }
+}
 
+// получение данных  и очистка локального хранилища
+form.addEventListener("submit", saveEmail);
+function saveEmail(evt) {
+  evt.preventDefault();
+
+  const inputEmail = form.elements.email.value;
+  const inputMessage = form.elements.message.value;
+  if (inputEmail && inputMessage !== "") {
+    let userForm = localStorage.getItem(LOCALSTORAGE_KEY);
+    userForm = JSON.parse(userForm);
+    console.log(userForm);
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+  evt.currentTarget.reset();  
+  } 
+  }
